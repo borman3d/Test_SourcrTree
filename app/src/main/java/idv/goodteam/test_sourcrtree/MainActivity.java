ad8ccfbf43fafb2ebdf1387182caee54644ecfd2
winston <winstonnewcat@gmail.com>
2020-04-05 19:31:26 +0800

    add function 1

---
+++ app/src/main/java/idv/goodteam/test_sourcrtree/MainActivity.java
@@ -0,0 +1,22 @@
+package idv.goodteam.test_sourcrtree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Function 1
+    void sayHello()
+    {
+
+    }
+}
